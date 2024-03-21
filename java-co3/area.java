b31650d4816c1ad3cd5856d7d5512cec2a23ce96
melwin7072 <146933485+melwin7072@users.noreply.github.com>
2024-03-21 19:56:16 +0530

    Add files via upload

---
+++ java-co3/area.java
@@ -0,0 +1,39 @@
+1. Area of different shapes using overloaded functions
+
+
+import java.util.Scanner;
+public class Overload
+{
+ void area (double r)
+ {
+ double Ar = 3.14*r*r;
+ System.out.println ("Area of Circle = "+Ar);
+ }
+ void area (int len, int br)
+ {
+ int Ar = len*br;
+ System.out.println("Area of Rectangle = "+ Ar);
+ }
+ void area (int l, int b, int h)
+ {
+ int Ar = l*b*h;
+ System.out.println("Area of Cuboid = "+ Ar);
+ }
+ public static void main(String[] args)
+ {
+ Scanner sc = new Scanner(System.in);
+ Overload obj1 = new Overload() ;
+ System.out.println ("Enter the Radius");
+ int r = sc.nextInt();
+ obj1.area(r);
+ System.out.println("Enter the Length & Breath ");
+ int len = sc.nextInt();
+ int br = sc.nextInt();
+ obj1.area(len,br);
+ System.out.println("Enter the Length, Breath & Height ");
+ int l = sc.nextInt();
+ int b = sc.nextInt();
+ int h = sc.nextInt();
+ obj1.area (l, b, h);
+ }
+} 
